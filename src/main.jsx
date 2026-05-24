4a82e7f233219b81d4baef424ea9d6c18fdeaf75
Uzair Ahmad <154722187+uzairatta@users.noreply.github.com>
2026-05-24 19:45:09 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import ProductDetail from './pages/ProductDetail.jsx'
 import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -13,7 +14,8 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<App />} />
         <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
-) 
+)
